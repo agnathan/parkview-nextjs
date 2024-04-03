5591edf71e5a0371a3117104b6e8dde3b5c26919
Daniel Holmlund <agnathan@gmail.com>
2024-04-02 19:51:23 -0700

    Added the firebase config information to the repo

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -7,18 +7,32 @@ import { getAnalytics } from 'firebase/analytics'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: 'AIzaSyDAIZzAGszVy9m5gLQT-Ud5BTydsVMHWhc',
+  authDomain: 'parkview-website.firebaseapp.com',
+  projectId: 'parkview-website',
+  storageBucket: 'parkview-website.appspot.com',
+  messagingSenderId: '252574190900',
+  appId: '1:252574190900:web:82b9d2ca7c44f85b406961',
+  measurementId: 'G-W8R9YJBGTE'
 }
 
-console.log(firebaseConfig)
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
 
-export default { analytics }
+// Firestore exports
+export const firestore = firebase.firestore()
+export const serverTimestamp = firebase.firestore.FieldValue.serverTimestamp
+export const fbTimestamp = firebase.firestore.Timestamp
+export const fromMillis = firebase.firestore.Timestamp.fromMillis
+export const increment = firebase.firestore.FieldValue.increment
+
+export const analytics = () => {
+  if (typeof window !== 'undefined') {
+    return firebase.analytics()
+  } else {
+    return null
+  }
+}
+export default firebase
